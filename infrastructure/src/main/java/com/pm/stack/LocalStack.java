0bda00edba5cc1d7d47e1907235b9c93929e39ab
tgrozenski <tyler.grozenski@gmail.com>
2025-09-24 13:34:56 -0700

    added beginning of IAC, generated cloudform template

---
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -0,0 +1,38 @@
+package com.pm.stack;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.AppProps;
+import software.amazon.awscdk.BootstraplessSynthesizer;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+
+public class LocalStack extends Stack {
+    private final Vpc vpc;
+
+    public LocalStack(final App scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        this.vpc = createVpc();
+    }
+
+    public static void main(final String[] args) {
+        App app = new App(
+                AppProps.builder().outdir("./cdk.out").build()
+        );
+        StackProps props = StackProps.builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+        new LocalStack(app, "localstack", props);
+        app.synth();
+        System.out.println("App synthesizing in progress...");
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
+    }
+}
