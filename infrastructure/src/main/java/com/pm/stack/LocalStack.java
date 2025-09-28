64323ebec54b759a73caacb788d65866fe483675
Tyler Grozenski <tyler.grozenski@gmail.com>
2025-09-28 13:29:56 -0700

    Created Infrastructure for ECS cluster and the AWS Fargate services

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -4,12 +4,17 @@ import software.amazon.awscdk.*;
 
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
-import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
-
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
@@ -30,6 +35,46 @@ public class LocalStack extends Stack {
         CfnCluster mskCluster = createMskCluster();
 
         this.ecsCluster = createEcsCluster();
+
+        FargateService authService = createFargateService(
+                "AuthService",
+                "auth-service",
+                List.of(4005),
+                authServiceDb,
+                Map.of("JWT_SECRET", "eW91ci1zdXBlci1zZWNyZXQta2V5LXRoYXQtaXMtbG9uZy1hbmQtc2VjdXJlCg=="));
+
+        authService.getNode().addDependency(authDbHealthCheck);
+        authService.getNode().addDependency(authServiceDb);
+
+        FargateService billingService =
+               createFargateService("Billing Service",
+                       "billing-service",
+                       List.of(4001, 9001),
+                       null,
+                       null);
+
+        FargateService analyticsService =
+                createFargateService("Analytics Service",
+                        "analytics-service",
+                        List.of(4002),
+                        null,
+                        null);
+        analyticsService.getNode().addDependency(mskCluster);
+
+        FargateService patientService =
+                createFargateService("Patient Service",
+                        "patient-service",
+                        List.of(4000),
+                        patientServiceDb,
+                        Map.of(
+                                "BILLING_SERVICE_ADDRESS", "host.docker.internal",
+                                "BILLING_SERVICE_GRPC_PORT", "9001"
+                        ));
+        patientService.getNode().addDependency(patientServiceDb);
+        patientService.getNode().addDependency(patientDbHealthCheck);
+        patientService.getNode().addDependency(billingService);
+        patientService.getNode().addDependency(mskCluster);
+
     }
 
 
@@ -89,6 +134,67 @@ public class LocalStack extends Stack {
                 .defaultCloudMapNamespace(CloudMapNamespaceOptions.builder().name("patient-management.local").build())
                 .build();
     }
+
+    private FargateService createFargateService(
+            String id,
+            String imageName,
+            List<Integer> ports,
+            DatabaseInstance db,
+            Map<String, String> additionalEnvVars) {
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, id + "Task")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        ContainerDefinitionOptions.Builder containerOptions =
+                ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromRegistry(imageName))
+                .portMappings(ports.stream()
+                        .map(port -> PortMapping.builder()
+                                .containerPort(port)
+                                .hostPort(port)
+                                .protocol(Protocol.TCP)
+                                .build())
+                            .toList())
+                .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                        .logGroup(LogGroup.Builder.create(this, id + "LogGroup")
+                                .logGroupName("/ecs/" + imageName)
+                                .removalPolicy(RemovalPolicy.DESTROY)
+                                .retention(RetentionDays.ONE_DAY)
+                                .build())
+                            .streamPrefix(imageName)
+                        .build()));
+
+        Map<String, String> envVars = new HashMap<>();
+        envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511, localhost.localstack.cloud:4512");
+
+        if (additionalEnvVars != null) {
+            envVars.putAll(additionalEnvVars);
+        }
+        if (db != null) {
+            envVars.put("SPRINT_DATASOURCE_URL", "jbc:postgresql://%s:%s/%s-db".formatted(
+                    db.getDbInstanceEndpointAddress(),
+                    db.getDbInstanceEndpointPort(),
+                    imageName
+            ));
+            envVars.put("SPRING_DATASOURCE_USERNAME", "admin_user");
+            envVars.put("SPRING_DATASOURCE_PASSWORD",
+                    db.getSecret().secretValueFromJson("passwods").toString());
+            envVars.put("SPRING_JPA_HIBERNATE_DDL_AUTO", "update");
+            envVars.put("SPRING_SQL_INIT_MODE", "always");
+            envVars.put("SPRING_DATASOURCE_HIKARI_INITIALIZATION_FAIL_TIMEOUT", "60000");
+        }
+        containerOptions.environment(envVars);
+        taskDefinition.addContainer(imageName + "Container", containerOptions.build());
+
+        return FargateService.Builder.create(this, id)
+                .cluster(ecsCluster)
+                .taskDefinition(taskDefinition)
+                .assignPublicIp(false)
+                .serviceName(imageName)
+                .build();
+    }
+
     public static void main(final String[] args) {
         App app = new App(
                 AppProps.builder().outdir("./cdk.out").build()
